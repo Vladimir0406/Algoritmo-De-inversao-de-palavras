8a8f955dd46c29057bd809d6bb50fa6ff9a7f1fc
Vladimir Valentim <164585255+Vladimir0406@users.noreply.github.com>
2024-06-26 15:47:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Variaveis
+String palavra, invertida = "";
+int i;
+
+// Entrada de dados
+Console.WriteLine("informe uma palavra");
+palavra = Console.ReadLine();
+
+// estrutura de repetiçao
+for (i = 0; i < palavra.Length;i++)
+    invertida = palavra[i] + invertida ;
+
+// Saida de dados
+Console.WriteLine($"a palavra invertida é :{invertida}");
